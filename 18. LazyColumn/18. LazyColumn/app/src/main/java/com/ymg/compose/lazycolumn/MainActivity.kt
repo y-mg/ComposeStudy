e75aba1519cc73a292d83d6e229ce84863583621
y-mg <ymg926@naver.com>
2023-04-26 14:15:16 +0900

    deploy

---
+++ 18. LazyColumn/18. LazyColumn/app/src/main/java/com/ymg/compose/lazycolumn/MainActivity.kt
@@ -0,0 +1,177 @@
+package com.ymg.compose.lazycolumn
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.ymg.compose.catalog.R
+import com.ymg.compose.lazycolumn.ui.theme.LazyColumnTheme
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LazyColumnTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    LazyColumnEx(items)
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun Item(itemData: ItemData) {
+    Card(
+        elevation = 8.dp,
+        modifier = Modifier.padding(4.dp),
+    ) {
+        Column(
+            modifier = Modifier.padding(8.dp)
+        ) {
+            Image(
+                painter = painterResource(id = itemData.imageId),
+                contentDescription = itemData.title
+            )
+            Spacer(
+                modifier = Modifier.size(8.dp)
+            )
+            Text(
+                text = itemData.title,
+                style = MaterialTheme.typography.h4
+            )
+            Spacer(
+                modifier = Modifier.size(8.dp)
+            )
+            Text(
+                text = itemData.description
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ItemPreview() {
+    LazyColumnTheme {
+        Item(
+            ItemData(
+                imageId = R.drawable.a1,
+                title = "해변 놀이 공원",
+                description = "해변 놀이 공원 설명입니다. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
+            )
+        )
+    }
+}
+
+
+@Composable
+fun LazyColumnEx(itemList: List<ItemData>) {
+    LazyColumn {
+        /*
+        item {
+            Item(itemList[0])
+        }
+        item {
+            Item(itemList[0])
+        }
+        */
+        items(itemList) {
+            Item(it)
+        }
+    }
+
+
+    /**
+     * Lazy 키워드는 필요할 때만 보여주겠다는 의미
+     *
+     * Header 혹은 Footer 가 필요한 경우 직접 LazyColumn 에 아이템을 넣으면 됨
+     *
+     * 여러 개의 아이템이 반복적으로 나와야 하는 경우에는 items 사용
+     */
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    LazyColumnTheme {
+        LazyColumnEx(items)
+    }
+}
+
+
+data class ItemData(
+    @DrawableRes val imageId: Int,
+    val title: String,
+    val description: String,
+)
+
+val items = listOf(
+    ItemData(
+        imageId = R.drawable.a1,
+        title = "해변 놀이 공원",
+        description = "해변 놀이 공원 설명입니다. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
+    ),
+    ItemData(
+        imageId = R.drawable.a2,
+        title = "캐년",
+        description = "미국의 캐년입니다. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
+    ),
+    ItemData(
+        imageId = R.drawable.a3,
+        title = "워터월드",
+        description = "워터월드입니다. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
+    ),
+    ItemData(
+        imageId = R.drawable.a4,
+        title = "미국의 캐년",
+        description = "미국의 캐년입니다. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
+    ),
+    ItemData(
+        imageId = R.drawable.a5,
+        title = "라스베가스",
+        description = "라스베가스입니다. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
+    ),
+    ItemData(
+        imageId = R.drawable.a6,
+        title = "호르슈 밴드",
+        description = "호르슈 밴드입니다. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
+    ),
+    ItemData(
+        imageId = R.drawable.a7,
+        title = "미국의 길",
+        description = "미국의 길입니다. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
+    ),
+    ItemData(
+        imageId = R.drawable.a8,
+        title = "엔텔로프 캐년",
+        description = "엔텔로프 캐년입니다. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
+    ),
+    ItemData(
+        imageId = R.drawable.a9,
+        title = "그랜드 캐년",
+        description = "그랜드 캐년입니다. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
+    ),
+)
