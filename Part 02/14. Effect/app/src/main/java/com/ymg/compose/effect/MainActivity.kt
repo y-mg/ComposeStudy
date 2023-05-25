b287158a7a330e21e676c8d82b910cb58e76b3d9
y-mg <ymg926@naver.com>
2023-05-25 16:28:05 +0900

    deploy

--- Part 02/14. Effect/app/src/main/java/com/ymg/compose/effect/MainActivity.kt
+++ Part 02/14. Effect/app/src/main/java/com/ymg/compose/effect/MainActivity.kt
@@ -1,14 +1,19 @@
 package com.ymg.compose.effect
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
 import androidx.lifecycle.LifecycleOwner
 import com.ymg.compose.effect.ui.theme.EffectTheme
 
@@ -33,10 +38,47 @@ class MainActivity : ComponentActivity() {
 fun EffectEx(lifecycleOwner: LifecycleOwner = LocalLifecycleOwner.current) {
     val scaffoldState = rememberScaffoldState()
 
-    /*Scaffold(
-        scaffoldState = scaffoldState
-    ) {
-    }*/
+    LaunchedEffect(scaffoldState.snackbarHostState) {
+        scaffoldState.snackbarHostState.showSnackbar(("Hello Compose"))
+    }
+
+    DisposableEffect(lifecycleOwner) {
+        val observer = LifecycleEventObserver { _, event ->
+            when (event) {
+                Lifecycle.Event.ON_START -> {
+                    Log.d("이펙트", "ON_START")
+                }
+
+                Lifecycle.Event.ON_RESUME -> {
+                    Log.d("이펙트", "ON_RESUME")
+                }
+
+                Lifecycle.Event.ON_PAUSE -> {
+                    Log.d("이펙트", "ON_PAUSE")
+                }
+
+                Lifecycle.Event.ON_STOP -> {
+                    Log.d("이펙트", "ON_STOP")
+                }
+
+                else -> {
+                    Log.d("이펙트", "그외")
+                }
+            }
+        }
+
+        lifecycleOwner.lifecycle.addObserver(observer)
+
+        onDispose {
+            lifecycleOwner.lifecycle.removeObserver(observer)
+        }
+    }
+
+
+    Scaffold(
+        scaffoldState = scaffoldState,
+        content = {}
+    )
 }
 
 @Preview(showBackground = true)
